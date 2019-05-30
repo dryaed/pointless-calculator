993c3b6e4461c0204d655db56537475f75a8a842
dryaed <51190317+dryaed@users.noreply.github.com>
2019-05-30 18:55:30 +0200

    Add files via upload
    Поколение 2. Сделал понятнее для пользователя, добавил выбор языка.

--- ax2bxcd.pas
+++ ax2bxcd.pas
@@ -1,20 +1,50 @@
-﻿program noname;
-var a, b, c, d, sqrtD, x1, x2: real;
-// sqrt
+﻿program D;
+var a, b, c, dd, sqrtD, x1, x2: real;
+    language: string;
+
 begin
+writeln ('Выберите свой язык / Choose your language');
+writeln ('"eng" - Английский / English');
+writeln ('"rus" - Русский / Russian ');
+write ('Ваш выбор: ');
+readln (language);
+if language='rus' then 
+begin
+writeln ('Решение квадратного уравнения (ax^2+bx+c=0)с помощью дискриминанта D');
 write ('Введите переменную а = ');
 readln (a);
 write ('Введите переменную b = ');
 readln (b);
 write ('Введите переменную с = ');
 readln (c);
-d:=(b*b)-(4*a*c);
-sqrtD:=sqrt(d);
+dd:=(b*b)-(4*a*c);
+sqrtD:=sqrt(dd);
 x1:=-b+sqrtD/2*a;
 x2:=-b-sqrtD/2*a;
-writeln ('Дискриминант = ',d);
+writeln ('Дискриминант (D) = ',dd);
 writeln ('x1 = ',x1);
 writeln ('x2 = ',x2);
-writeln ('Нажмите любую кнопку для выхода из программы');
+writeln ('Нажмите Enter для завершения программы');
 readln ();
+end
+else
+if language='eng' then 
+begin
+writeln ('Solving quadratic equation (ax^2+bx+c=0) with D (b^2-4ac)');
+write ('Enter variable а = ');
+readln (a);
+write ('Enter variable b = ');
+readln (b);
+write ('Enter variable с = ');
+readln (c);
+dd:=(b*b)-(a*c);
+sqrtD:=sqrt(dd);
+x1:=-b+sqrtD/2*a;
+x2:=-b-sqrtD/2*a;
+writeln ('Discriminant (D) = ',dd);
+writeln ('x1 = ',x1);
+writeln ('x2 = ',x2);
+writeln ('Press Enter button to end the app');
+readln ();
+end
 end.
--- ax2bxcd1.pas
+++ ax2bxcd1.pas
@@ -1,7 +1,16 @@
-﻿program noname;
+﻿program D4;
 var a, b, c, k, d1, sqrtD1, x1, x2: real;
+    language: string;
 
 begin
+writeln ('Выберите свой язык / Choose your language');
+writeln ('"eng" - Английский / English');
+writeln ('"rus" - Русский / Russian ');
+write ('Ваш выбор: ');
+readln (language);
+if language='rus' then 
+begin
+writeln ('Решение квадратного уравнения (ax^2+bx+c=0)с помощью дискриминанта D');
 write ('Введите переменную а = ');
 readln (a);
 write ('Введите переменную b = ');
@@ -16,6 +25,25 @@ x2:=-k-sqrtD1/2*a;
 writeln ('Дискриминант (D/4) = ',d1);
 writeln ('x1 = ',x1);
 writeln ('x2 = ',x2);
-writeln ('Нажмите любую кнопку для завершения программы');
+writeln ('Нажмите Enter для завершения программы');
+readln ();
+end
+else 
+writeln ('Solving quadratic equation (ax^2+bx+c=0) with D/4 (k^2-ac)');
+write ('Enter variable а = ');
+readln (a);
+write ('Enter variable b = ');
+readln (b);
+write ('Enter variable с = ');
+readln (c);
+k:=b/2;
+d1:=(k*k)-(a*c);
+sqrtD1:=sqrt(d1);
+x1:=-k+sqrtD1/2*a;
+x2:=-k-sqrtD1/2*a;
+writeln ('Discriminant (D/4) = ',d1);
+writeln ('x1 = ',x1);
+writeln ('x2 = ',x2);
+writeln ('Press Enter button to end the app');
 readln ();
 end.
