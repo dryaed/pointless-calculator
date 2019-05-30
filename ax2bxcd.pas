f81054591d7dddc501a43c128fc3118ded4259ad
dryaed <51190317+dryaed@users.noreply.github.com>
2019-05-30 09:37:21 +0200

    Первое поколение
    Отдельные пути решения в две программы

---
+++ ax2bxcd.pas
@@ -0,0 +1,20 @@
+﻿program noname;
+var a, b, c, d, sqrtD, x1, x2: real;
+// sqrt
+begin
+write ('Введите переменную а = ');
+readln (a);
+write ('Введите переменную b = ');
+readln (b);
+write ('Введите переменную с = ');
+readln (c);
+d:=(b*b)-(4*a*c);
+sqrtD:=sqrt(d);
+x1:=-b+sqrtD/2*a;
+x2:=-b-sqrtD/2*a;
+writeln ('Дискриминант = ',d);
+writeln ('x1 = ',x1);
+writeln ('x2 = ',x2);
+writeln ('Нажмите любую кнопку для выхода из программы');
+readln ();
+end.
---
+++ ax2bxcd1.pas
@@ -0,0 +1,21 @@
+﻿program noname;
+var a, b, c, k, d1, sqrtD1, x1, x2: real;
+
+begin
+write ('Введите переменную а = ');
+readln (a);
+write ('Введите переменную b = ');
+readln (b);
+write ('Введите переменную с = ');
+readln (c);
+k:=b/2;
+d1:=(k*k)-(a*c);
+sqrtD1:=sqrt(d1);
+x1:=-k+sqrtD1/2*a;
+x2:=-k-sqrtD1/2*a;
+writeln ('Дискриминант (D/4) = ',d1);
+writeln ('x1 = ',x1);
+writeln ('x2 = ',x2);
+writeln ('Нажмите любую кнопку для завершения программы');
+readln ();
+end.
